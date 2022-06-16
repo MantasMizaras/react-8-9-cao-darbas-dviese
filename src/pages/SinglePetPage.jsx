8fd1924379c2742540053938bb8914f7140cbca4
Mantas M <mantasef@gmail.com>
2022-06-16 15:38:00 +0300

    message added

--- src/pages/SinglePetPage.jsx
+++ src/pages/SinglePetPage.jsx
@@ -7,9 +7,7 @@ const SinglePetPage = () => {
   const { petId } = useParams();
 
   const getCurrentPetAndSave = async () => {
-    const resp = await fetch(
-      `https://glittery-dull-snickerdoodle.glitch.me/v1/logs/${petId}`
-    );
+    const resp = await fetch(`https://glittery-dull-snickerdoodle.glitch.me/v1/logs/${petId}`);
     const dataInJs = await resp.json();
     const currentPet = dataInJs.find(({ id }) => id === +petId);
     setCurrentPet(currentPet);
@@ -19,6 +17,26 @@ const SinglePetPage = () => {
   useEffect(() => {
     getCurrentPetAndSave();
   }, []);
+
+  if (currentPet !== undefined) {
+    return (
+      <div className='pet-card'>
+        <h2>{currentPet.name}</h2>
+        <p>{currentPet.status}</p>
+        <p>{currentPet.description}</p>
+        <Link to={`/pets/${currentPet.id}`}>
+          <button>View Log</button>
+        </Link>
+        <button>Delete</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className='pet-card'>
+        <h3>nera jokiu irasu</h3>
+      </div>
+    );
+  }
 
   return (
     <div className='pet-card'>
